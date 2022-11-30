22c52f8e6374725e61e8a3a4b933504ad2cb75aa
Adrian <adrian.berindie02@e-uvt.ro>
2022-11-30 17:22:43 +0200

    test

---
+++ Factura.java
@@ -0,0 +1,8 @@
+public class Factura {
+    private String nume;
+    private Tip tip;
+    private String Firma;
+    private double suma_platita;
+    private double suma;
+    private String id;
+}
---
+++ Tip.java
@@ -0,0 +1,3 @@
+public enum Tip {
+    Gaz,Apa,Curent,Produse
+}
